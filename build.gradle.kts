453fd34bd4f426c4b0cdf0e8c0aa4bd0ac22b869
Naresh Deeti <nareshdeetind@outlook.com>
2025-05-07 10:49:33 +0530

    Add windows basic config

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,10 +30,23 @@ tasks.jar {
 tasks.register<Exec>("buildNative") {
     dependsOn("build")
     workingDir = file("$projectDir")
+    val osName = System.getProperty("os.name")
+    var jpackageCommand = ""
+    var shellType = "bash"
+    var secondArg = "-c"
+    val vendorName = "Naresh Deeti"
+    if (osName.contains("windows", true)) {
+        shellType = "cmd"
+        secondArg = "/c"
+        jpackageCommand =
+            "jpackage --input build/libs/ -n cwd --main-jar cwd-0.0.1-SNAPSHOT.jar --main-class com.ndeeti.cwd.CwdApplication -t exe -d build/"
+    } else {
+        jpackageCommand =
+            "jpackage --input build/libs/ -n cwd --main-jar cwd-0.0.1-SNAPSHOT.jar --main-class com.ndeeti.cwd.CwdApplication -t rpm -d build/"
+    }
     commandLine(
-        "bash",
-        "-c",
-        "jpackage --input build/libs/ -n cwd --main-jar cwd-0.0.1-SNAPSHOT.jar --main-class com.ndeeti.cwd.CwdApplication -t rpm -d build/"
+        shellType, secondArg,
+        jpackageCommand
     ).doFirst {
         println("Crafting Native executable")
     }
